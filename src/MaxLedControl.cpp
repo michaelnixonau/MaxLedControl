5792d4590633e276f85c4de71debab8e2ce67a8e
Michael Nixon <michael@edukits.co>
2025-11-30 19:15:07 -0800

    feat: move opcodes to lib header

--- src/MaxLedControl.cpp
+++ src/MaxLedControl.cpp
@@ -27,22 +27,6 @@
 
 #include "MaxLedControl.h"
 
-//the opcodes for the MAX7221 and MAX7219
-#define OP_NOOP   0
-#define OP_DIGIT0 1
-#define OP_DIGIT1 2
-#define OP_DIGIT2 3
-#define OP_DIGIT3 4
-#define OP_DIGIT4 5
-#define OP_DIGIT5 6
-#define OP_DIGIT6 7
-#define OP_DIGIT7 8
-#define OP_DECODEMODE  9
-#define OP_INTENSITY   10
-#define OP_SCANLIMIT   11
-#define OP_SHUTDOWN    12
-#define OP_DISPLAYTEST 15
-
 LedControl::LedControl(int dataPin, int clkPin, int csPin, int numDevices)
     : Adafruit_GFX(numDevices * 8, 8), SPI_MOSI(dataPin), SPI_CLK(clkPin), SPI_CS(csPin), maxDevices(numDevices) {
     SPI_MOSI=dataPin;
--- src/MaxLedControl.h
+++ src/MaxLedControl.h
@@ -29,13 +29,26 @@
 
 #include <avr/pgmspace.h>
 
-#if (ARDUINO >= 100)
 #include <Arduino.h>
-#else
-#include <WProgram.h>
-#endif
-
 #include <Adafruit_GFX.h>
+
+/**
+ * Opcodes for the MAX7221 and MAX7219
+ */
+#define OP_NOOP   0
+#define OP_DIGIT0 1
+#define OP_DIGIT1 2
+#define OP_DIGIT2 3
+#define OP_DIGIT3 4
+#define OP_DIGIT4 5
+#define OP_DIGIT5 6
+#define OP_DIGIT6 7
+#define OP_DIGIT7 8
+#define OP_DECODEMODE  9
+#define OP_INTENSITY   10
+#define OP_SCANLIMIT   11
+#define OP_SHUTDOWN    12
+#define OP_DISPLAYTEST 15
 
 /*
  * Segments to be switched on for characters and digits on
